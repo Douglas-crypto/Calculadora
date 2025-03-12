b62440b1003a7bdafb51d6507c2a05df9ede733d
DOUGLAS DA COSTA <rakamaru123@gmail.com>
2025-03-11 21:29:48 -0300

    Add files via upload

---
+++ Calculadora/src/main/java/org/example/Opcoes/Opicoes/Operacoes.java
@@ -0,0 +1,27 @@
+package org.example.Opicoes;
+
+public enum Operacoes {
+    SOMA("+"),
+    SUBTRACAO("-"),
+    DIVISAO("/"),
+    MULTIPLICACAO("*");
+
+    private String simbolo;
+
+    Operacoes(String simbolo) {
+        this.simbolo = simbolo;
+    }
+
+    public String getSimbolo() {
+        return simbolo;
+    }
+
+    public static Operacoes fromSimbolo(String simbolo) {
+        for (Operacoes operacao : Operacoes.values()) {
+            if (operacao.getSimbolo().equals(simbolo)) {
+                return operacao;
+            }
+        }
+        throw new IllegalArgumentException("Operação inválida: " + simbolo);
+    }
+}
